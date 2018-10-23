b608a79304b4f20ce95c6d476b02cc9db1b1d3f1
canisterism <ohtthstc@gmail.com>
2018-10-23 23:43:30 +0900

    [add]first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button v-on:click="login">Connection</button>
+    <p>You don't have account? You can <router-link to="/sign-up">create one</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function () {
+    return {}
+  },
+  methods: {
+    login: function () {
+      this.$router.replace('hello-world')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login {
+    margin-top: 40px;
+  }
+  input {
+    display: block;
+    margin: 10px auto;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sign-up">
+    <h3>Let's create a new account!</h3>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button>Sign Up</button>
+    <span>Or go back to <router-link to="/login">login</router-link>.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signUp',
+  data: function () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+  .signUp {
+    margin-top: 40px;
+  }
+  input {
+    display: block;
+    margin: 10px auto;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 10px;
+    width: 20%;
+    padding: 15px;
+  }
+  span {
+    display: block;
+    margin-top: 20px;
+    font-size: 11px;
+  }
+</style>
